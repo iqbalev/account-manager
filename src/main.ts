f22161df1d65a170d27bbab6e0b0706d9eb3467c
Muhammad Iqbal <52034320+iqbalev@users.noreply.github.com>
2025-11-18 23:13:37 +0700

    Fix render logic and organize code
    - Previously, submitting a new form re-rendered the entire array.
    - Now, each submit only renders the submitted form.
    - Render the entire array only on the first load (though no persistent storage yet).
    - Improve naming, code separation, and types.

--- src/main.ts
+++ src/main.ts
@@ -5,61 +5,126 @@ type Account = {
   password: string;
 };
 type Accounts = Account[];
+type AccountListItemParagraphs = {
+  labelParagraph: HTMLParagraphElement;
+  emailParagraph: HTMLParagraphElement;
+  usernameParagraph: HTMLParagraphElement;
+  passwordParagraph: HTMLParagraphElement;
+};
 
 const accounts: Accounts = [];
-const searchInputEl = document.querySelector(".search") as HTMLInputElement;
-const formEl = document.querySelector(".form") as HTMLFormElement;
-const labelInputEl = document.querySelector(".label") as HTMLInputElement;
-const emailInputEl = document.querySelector(".email") as HTMLInputElement;
-const usernameInputEl = document.querySelector(".username") as HTMLInputElement;
-const passwordInputEl = document.querySelector(".password") as HTMLInputElement;
-const accountsUlEl = document.querySelector(".accounts") as HTMLUListElement;
+const searchInput = document.querySelector(".search") as HTMLInputElement;
+const form = document.querySelector(".form") as HTMLFormElement;
+const labelInput = document.querySelector(".label") as HTMLInputElement;
+const emailInput = document.querySelector(".email") as HTMLInputElement;
+const usernameInput = document.querySelector(".username") as HTMLInputElement;
+const passwordInput = document.querySelector(".password") as HTMLInputElement;
+const accountsList = document.querySelector(".accounts") as HTMLUListElement;
 
-function searchAccounts() {
-  const searchQuery = searchInputEl.value.toLowerCase();
-  const lists = accountsUlEl.getElementsByTagName("li");
-  for (const list of lists) {
-    if (list.textContent.toLowerCase().includes(searchQuery)) {
-      list.style.display = "block";
+function searchAccounts(): void {
+  const searchQuery = searchInput.value.toLowerCase();
+  const accountListItems = accountsList.getElementsByTagName("li");
+
+  for (const accountListItem of accountListItems) {
+    const isResultsFound = accountListItem.textContent
+      .toLowerCase()
+      .includes(searchQuery);
+
+    if (isResultsFound) {
+      accountListItem.style.display = "block";
     } else {
-      list.style.display = "none";
+      accountListItem.style.display = "none";
     }
   }
 }
 
-function addAccount(e: SubmitEvent) {
-  e.preventDefault();
+function addAccount(accounts: Accounts): Accounts {
   const account: Account = {
-    label: labelInputEl.value,
-    email: emailInputEl.value,
-    username: usernameInputEl.value,
-    password: passwordInputEl.value,
+    label: labelInput.value,
+    email: emailInput.value,
+    username: usernameInput.value,
+    password: passwordInput.value,
   };
+
   accounts.push(account);
-  renderAccounts();
+  return accounts;
 }
 
-function renderAccounts() {
-  accountsUlEl.innerHTML = "";
-  accounts.forEach((account) => {
-    const liEl = document.createElement("li") as HTMLLIElement;
-    const labelEl = document.createElement("p") as HTMLParagraphElement;
-    const emailEl = document.createElement("p") as HTMLParagraphElement;
-    const usernameEl = document.createElement("p") as HTMLParagraphElement;
-    const passwordEl = document.createElement("p") as HTMLParagraphElement;
-    liEl.classList.add("account");
-    labelEl.classList.add("label");
-    emailEl.classList.add("email");
-    usernameEl.classList.add("username");
-    passwordEl.classList.add("password");
-    labelEl.textContent = account.label;
-    emailEl.textContent = account.email;
-    usernameEl.textContent = account.username;
-    passwordEl.textContent = account.password;
-    liEl.append(labelEl, emailEl, usernameEl, passwordEl);
-    accountsUlEl.append(liEl);
-  });
+function createListItem(className: string): HTMLLIElement {
+  const listItem = document.createElement("li");
+  listItem.classList.add(className);
+  return listItem;
 }
 
-searchInputEl.addEventListener("input", searchAccounts);
-formEl.addEventListener("submit", addAccount);
+function createAccountListItemParagraphs(
+  account: Account
+): AccountListItemParagraphs {
+  const labelParagraph = document.createElement("p");
+  const emailParagraph = document.createElement("p");
+  const usernameParagraph = document.createElement("p");
+  const passwordParagraph = document.createElement("p");
+
+  labelParagraph.classList.add("label");
+  emailParagraph.classList.add("email");
+  usernameParagraph.classList.add("username");
+  passwordParagraph.classList.add("password");
+
+  labelParagraph.textContent = account.label;
+  emailParagraph.textContent = account.email;
+  usernameParagraph.textContent = account.username;
+  passwordParagraph.textContent = account.password;
+
+  return {
+    labelParagraph,
+    emailParagraph,
+    usernameParagraph,
+    passwordParagraph,
+  };
+}
+
+function renderAllAccounts(): void {
+  accountsList.innerHTML = "";
+
+  if (accounts.length > 0) {
+    accounts.forEach((account) => {
+      const accountListItem = createListItem("account");
+      const accountListItemParagraphs =
+        createAccountListItemParagraphs(account);
+
+      accountListItem.append(
+        accountListItemParagraphs.labelParagraph,
+        accountListItemParagraphs.emailParagraph,
+        accountListItemParagraphs.usernameParagraph,
+        accountListItemParagraphs.passwordParagraph
+      );
+
+      accountsList.append(accountListItem);
+    });
+  } else {
+    console.log("Still empty here...");
+  }
+}
+
+function renderNewAccount(): void {
+  const accountListItem = createListItem("account");
+  const accountListItemParagraphs = createAccountListItemParagraphs(
+    accounts[accounts.length - 1]
+  );
+
+  accountListItem.append(
+    accountListItemParagraphs.labelParagraph,
+    accountListItemParagraphs.emailParagraph,
+    accountListItemParagraphs.usernameParagraph,
+    accountListItemParagraphs.passwordParagraph
+  );
+
+  accountsList.append(accountListItem);
+}
+
+document.addEventListener("DOMContentLoaded", renderAllAccounts);
+searchInput.addEventListener("input", searchAccounts);
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addAccount(accounts);
+  renderNewAccount();
+});
